2ca9123ac9b0fb7318e8fbab8a5c733b68e449ac
RajashriMahato <89263509+RajashriMahato@users.noreply.github.com>
2022-05-21 01:31:22 +0530

    Update UltrasonicSensor.ino

--- UltrasonicSensor.ino
+++ UltrasonicSensor.ino
@@ -1,27 +1,20 @@
-// ---------------------------------------------------------------- //
-// Arduino Ultrasoninc Sensor HC-SR04
-// Re-writed by Arbi Abdul Jabbaar
-// Using Arduino IDE 1.8.7
-// Using HC-SR04 Module
-// Tested on 17 September 2019
-// ---------------------------------------------------------------- //
 
 #define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
 
-// defines variables
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+// defining the variables
+long duration; 
+int distance;
 
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
-  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
-  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
-  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600); 
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); 
   Serial.println("with Arduino UNO R3");
 }
 void loop() {
-  // Clears the trigPin condition
+  // Clears the condition of the trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
